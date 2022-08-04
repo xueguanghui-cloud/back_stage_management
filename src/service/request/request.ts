4f905e0b3e6e2e3ba4a15f43d41387803d68914a
codexgh <1239425965@qq.com>
2022-08-04 18:29:29 +0800

    feat(service): 使用typescript封装axios

--- src/service/request/request.ts
+++ src/service/request/request.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import type { AxiosInstance, AxiosRequestConfig } from 'axios'
+import type { AxiosInstance } from 'axios'
 import type { GHRequestInterceptors, GHRequestConfig } from './type'
 
 class GHRequest {
